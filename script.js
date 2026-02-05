891ea0b4ea2949b4bf6823f18739308dcda76830
Mohamed <mohamed@example.com>
2026-02-05 18:16:03 +0100

    changment de prenom

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // PERSONNALISATION
-const personName = "Fatou"; // Change le prénom ici
+const personName = "Adja"; // Change le prénom ici
 const gifUrl = "IMG_3151.jpg"; // Image célébration locale
 
 // Elements
