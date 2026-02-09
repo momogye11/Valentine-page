5a9a2c99ea7061d8e94d4b8e641c71f4129b4df6
Mohamed <mohamed@example.com>
2026-02-09 19:12:16 +0100

    changement

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // PERSONNALISATION
-const personName = "Bamby"; // Change le prénom ici
+const personName = "Sapphire"; // Change le prénom ici
 const gifUrl = "IMG_3151.jpg"; // Image célébration locale
 
 // Elements
