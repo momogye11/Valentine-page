b3f6fa31f6057e6f83ca9c29b6c24f8e6ab21dcb
Mohamed <mohamed@example.com>
2026-05-13 00:53:37 +0200

    changement nom

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // PERSONNALISATION
-const personName = "Aisha"; // Change le prénom ici
+const personName = "Rita"; // Change le prénom ici
 const gifUrl = "IMG_3151.jpg"; // Image célébration locale
 
 // Elements
